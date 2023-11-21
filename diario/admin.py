14e749560855ed3faacdbdfd7b238efeb70d702c
Jair Figueiredo <jairfigueiredo@gmail.com>
2023-11-20 21:59:02 -0500

    feat: ajuste para lançamento de presenças

--- diario/admin.py
+++ diario/admin.py
@@ -15,16 +15,22 @@ from .models import Professor, Aluno, Modulo, Turma, AulaAgendada, Presenca
 #         verbose_name = U'Membro de Comissão'
 #         verbose_name_plural = U'Membros de Comissão'
 
+class TurmaInline(admin.TabularInline):
+    model = Turma
+    extra = 1
+    allow_add = False
+
 
 class ProfessorAdmin(admin.ModelAdmin):
     list_display = ('nome', 'email', 'telefone', 'usuario')
     search_fields = ('nome', 'email', 'telefone')
 
+    inlines = [TurmaInline]
+
 
 class AlunoAdmin(admin.ModelAdmin):
     list_display = ('nome', 'email', 'telefone')
     search_fields = ('nome', 'email', 'telefone')
-    inlines = []
 
 
 class ModuloAdmin(admin.ModelAdmin):
@@ -32,9 +38,22 @@ class ModuloAdmin(admin.ModelAdmin):
     search_fields = ('nome', 'descricao', 'trilha')
 
 
+class AulasInline(admin.TabularInline):
+    model = AulaAgendada
+    extra = 1
+    allow_add = True
+
+
+class AlunosInline(admin.TabularInline):
+    model = Turma.alunos.through
+    extra = 1
+
+
 class TurmaAdmin(admin.ModelAdmin):
     list_display = ('nome', 'professor', 'modulo')
     search_fields = ('nome', 'professor', 'modulo')
+
+    inlines = [AulasInline, AlunosInline, ]
 
 
 class AulaAdmin(admin.ModelAdmin):
@@ -43,8 +62,8 @@ class AulaAdmin(admin.ModelAdmin):
 
 
 class PresencaAdmin(admin.ModelAdmin):
-    list_display = ('aula', 'aluno', 'presenca', 'justificativa')
-    search_fields = ('aula', 'aluno', 'presenca', 'justificativa')
+    list_display = ('aula', 'aluno', 'situacao', 'justificativa')
+    search_fields = ('aula', 'aluno', 'situacao', 'justificativa')
 
 
 admin.site.register(Professor, ProfessorAdmin)
--- diario/models.py
+++ diario/models.py
@@ -31,9 +31,9 @@ class Modulo(models.Model):
 
 class Turma(models.Model):
     nome = models.CharField(max_length=255, null=False, blank=False)
+    modulo = models.ForeignKey(Modulo, on_delete=models.CASCADE)
     professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
     alunos = models.ManyToManyField(Aluno)
-    modulo = models.ForeignKey(Modulo, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.nome
@@ -45,18 +45,38 @@ class AulaAgendada(models.Model):
     hora_inicio = models.TimeField(null=True, blank=True)
     hora_fim = models.TimeField(null=True, blank=True)
 
+    def get_alunos_matriculados(self):
+        return self.turma.alunos.all()
+
     def __str__(self):
         return self.turma.modulo.nome + ' - ' + self.data.strftime('%d/%m/%Y')
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        alunos_turma = self.turma.alunos.all()
+        for aluno in alunos_turma:
+            Presenca.objects.create(aula=self, aluno=aluno)
+
+# @receiver(post_save, sender=AulaAgendada)
+# def criar_presencas(sender, instance, **kwargs):
+#     # Verifique se a aula foi recém-criada para evitar chamadas desnecessárias
+#     if kwargs.get('created', False):
+#         # Obtenha todos os alunos da turma
+#         alunos_turma = instance.turma.alunos.all()
+#         # Crie instâncias de Presenca para cada aluno da turma
+#         for aluno in alunos_turma:
+#             Presenca.objects.create(aula=instance, aluno=aluno)
 
 
 class Presenca(models.Model):
     aula = models.ForeignKey(AulaAgendada, on_delete=models.CASCADE)
     aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
-    presenca = models.BooleanField(null=False, blank=False)
+    situacao = models.CharField(max_length=1, choices=[('P', 'Presente'), ('F', 'Faltou'),
+                                                       ('J', 'Justificada')], null=True, blank=True)
     justificativa = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
-        return self.aluno + ' - ' + self.aula.turma.nome + ' - ' + self.aula.data.strftime('%d/%m/%Y') + ' - ' + self.presenca
+        return self.aluno.nome + ' - ' + self.aula.turma.nome + ' - ' + self.aula.data.strftime('%d/%m/%Y')
 
 
 class Avaliacao(models.Model):
