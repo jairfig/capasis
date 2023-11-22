dcab30a49be1102ccdfafde5e17701d0fa5702b7
Jair Figueiredo <jairfigueiredo@gmail.com>
2023-11-22 16:45:28 -0500

    Feat: Organização inicial do diário

--- diario/views.py
+++ diario/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def meu_diario(request):
-    return render(request, 'diario/lista_alunos.html')
+    return render(request, 'diario/meu_diario.html')
