8dac9e1759f778a1886ea03033846524fcd0bf58
Jair Figueiredo <jairfigueiredo@gmail.com>
2024-01-09 16:18:54 -0500

    Fix: Salvando as presenças

--- diario/forms.py
+++ diario/forms.py
@@ -4,23 +4,15 @@ from django.forms.widgets import TextInput, CheckboxInput
 from .models import Presenca
 
 
-class ReadOnlyWidget(TextInput):
-    def render(self, name, value, attrs=None, renderer=None):
-        return value or super().render(name, value, attrs)
-
-
 class PresencaForm(forms.ModelForm):
     class Meta:
         model = Presenca
         fields = ['aluno', 'presente', 'justificativa']
-        widgets = {'presente': forms.CheckboxInput(),
-                   # 'aluno': ReadOnlyWidget(attrs={'readonly': 'readonly'}),
-                   }
+        widgets = {'presente': forms.CheckboxInput(),}
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # self.fields['aluno'].widget.attrs['readonly'] = 'readonly'
-        # self.fields['aluno'].disabled = True
+        self.fields['aluno'].disabled = True
+
 
 PresencaFormSet = modelformset_factory(Presenca, form=PresencaForm, extra=0)
-# PresencaFormSet = modelformset_factory(Presenca, fields=['aluno', 'presente', 'justificativa'], extra=0)
