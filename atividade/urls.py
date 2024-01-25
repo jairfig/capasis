8b92cb7d897a09ceceecf33a10f3af124848a90e
João Félix <joao.vidal@sou.ufac.br>
2024-01-24 19:55:07 -0500

    Atividade

--- atividade/urls.py
+++ atividade/urls.py
@@ -3,4 +3,6 @@ from atividade import views
 
 urlpatterns = [
     path('', views.atividade, name='atividade'),
+    path('adicionar', views.atividadeAdd, name='atividadeAdd'),
+    path('form', views.atividadeForm, name='atividadeForm'),
 ]
--- atividade/views.py
+++ atividade/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def atividade(request):
     return render(request, 'atividade.html')
+
+def atividadeAdd(request):
+    return render(request, 'atividadeAdd.html')
+
+def atividadeForm(request):
+    return render(request, 'atividadeForm.html')
